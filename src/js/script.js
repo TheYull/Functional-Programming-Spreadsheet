24a2e2b426a14af45694156c775624841a59f568
Yuliia <the.smile.too@gmail.com>
2024-05-17 20:47:58 +0300

    add variables sum, isEven

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+const isEven = (num) => num % 2 === 0;
+const sum = (nums) => nums.reduce((acc, el) => acc + el, 0);
 const range = (start, end) => Array(end - start + 1).fill(start);
 
 const charRange = (start, end) =>
