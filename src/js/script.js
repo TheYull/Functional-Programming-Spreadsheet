b90be24dbb561bfba6a3c90231a264354766abcf
Yuliia <the.smile.too@gmail.com>
2024-05-23 10:45:30 +0300

    add variable "update"

--- src/js/script.js
+++ src/js/script.js
@@ -40,7 +40,15 @@ window.onload = () => {
       input.type = "text";
       input.id = letter + number;
       input.ariaLabel = letter + number;
+      input.onchange = update;
       container.appendChild(input);
     });
   });
+};
+
+const update = (event) => {
+  const element = event.target;
+  const value = element.value.replace(/\s/g, "");
+  if (!value.includes(element.id)) {
+  }
 };
